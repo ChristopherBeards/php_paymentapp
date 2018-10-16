9ceb1926f625ee792b65905d75a93bd1e47dbec7
Christopher Beards <BeardsCM@gmail.com>
2018-10-16 14:36:03 -0400

    added composer dependency, also implemented a sanitize function and stored form values to variables; gitignore added

--- charge.php
+++ charge.php
@@ -1,2 +1,13 @@
 <?php 
-  echo 'SUBMITTED';
+  require_once('vendor/autoload.php');
+
+  \Stripe\Stripe::setApiKey('sk_test_qDdl7G3Rc54lyewIdZP5ZHD0');
+
+  // Sanitize form inputs
+  $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
+
+  // Get form data
+  $first_name = $POST['first_name'];
+  $last_name = $POST['last_name'];
+  $email = $POST['email'];
+  $token = $POST['stripeToken'];
